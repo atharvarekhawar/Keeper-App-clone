7372e0f1864124a5e3624d8208078a0e4d1474b9
Atharva Rekhawar <atharvarekhawar64@gmail.com>
2022-11-16 01:01:35 +0530

    first commit

---
+++ build/precache-manifest.024ed994181852789401a114f2959031.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8c6dfc40fc05d5b08d0ff2a51d95b4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1eadfdca23b79cb9b9b7",
+    "url": "/static/js/2.aa415f1b.chunk.js"
+  },
+  {
+    "revision": "1d78a2824da9ccb85ca5",
+    "url": "/static/js/main.a0b45ab7.chunk.js"
+  },
+  {
+    "revision": "84832877ed76605d723e",
+    "url": "/static/js/runtime-main.aa743321.js"
+  }
+]);
